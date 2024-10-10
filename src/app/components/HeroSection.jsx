1bdca3ee9a41506f503584f7292df882b16989d7
adnanfrd <adnanmadhar74@gmail.com>
2024-10-10 08:36:23 -0700

    changes logo name

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,20 +15,22 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-4 text-4xl sm:text-6xl lg:text-5xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
             <TypeAnimation
               sequence={[
+                "M Adnan Fareed",
+                1000,
                 "Full Stack Developer",
                 1000,
                 "MERN Stack Expert",
                 1000,
-                "Backend with Node.js",
+                "Backend with NodeJS",
                 1000,
-                "Frontend with React.js",
+                "Frontend with ReactJS/NextJS",
                 1000,
               ]}
               wrapper="span"
